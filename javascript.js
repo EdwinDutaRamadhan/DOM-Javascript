72e44882df637ab6e23ddf43c5d43b7c2fc564cb
EdwinDutaRamadhan <89692738+EdwinDutaRamadhan@users.noreply.github.com>
2023-01-04 22:22:06 +0700

    DOM

--- javascript.js
+++ javascript.js
@@ -1,22 +1,34 @@
-const link = document.querySelector('section#a a');
-link.removeAttribute('href');
+const mainButton = document.querySelector('button[name="mainButton"]');
+const randomButton = document.querySelector('button[name="randomButton"]');
+mainButton.onclick = () =>{
+    document.body.style.background = 'red';
+};
+randomButton.onclick = () =>{
+    const r = Math.round(Math.random()*255);
+    const g = Math.round(Math.random()*255);
+    const b = Math.round(Math.random()*255);
+    document.body.style.background = `rgb(${r},${g},${b})`;
+};
 
-const p2 = document.querySelector('.p2');
+const redRange = document.querySelector('input[name="redRange"]');
+const greenRange = document.querySelector('input[name="greenRange"]');
+const blueRange = document.querySelector('input[name="blueRange"]');
 
-p2.addEventListener('mouseenter', () =>{
-    p2.style.backgroundColor = 'lightblue';
+redRange.addEventListener('input', () => {
+    const r = redRange.value;
+    const g = greenRange.value;
+    const b = blueRange.value;
+    document.body.style.background = `rgb(${r},${g},${b})`;
 });
-p2.addEventListener('mouseout', () =>{
-    p2.style.backgroundColor = 'white';
+greenRange.addEventListener('input', () => {
+    const r = redRange.value;
+    const g = greenRange.value;
+    const b = blueRange.value;
+    document.body.style.background = `rgb(${r},${g},${b})`;
 });
-
-const p4 = document.querySelector('section#b p');
-
-p4.addEventListener('click', () =>{
-    const ul = document.querySelector('section#b ul');
-    const liBaru = document.createElement('li');
-    const textPBaru = document.createTextNode('Ini Text Li Baru');
-    liBaru.appendChild(textPBaru);
-    ul.appendChild(liBaru);
+blueRange.addEventListener('input', () => {
+    const r = redRange.value;
+    const g = greenRange.value;
+    const b = blueRange.value;
+    document.body.style.background = `rgb(${r},${g},${b})`;
 });
-
